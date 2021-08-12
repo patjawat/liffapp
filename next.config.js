3d2bb7117543a130d15e1907f5ba0e884644c149
patjawat <patjawat@gmail.com>
2021-08-12 19:43:19 +0700

    update

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+        NEXT_PUBLIC_LIFF_ID: '1656313134-na2Ad8MB',
+    },
+  }
--- pages/about.js
+++ pages/about.js
@@ -2,22 +2,20 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-// import ProTip from '../src/ProTip';
+import Button from '@material-ui/core/Button';
 import Link from '../components/Link';
 // import Copyright from '../src/Copyright';
-import Navbar from '@/components/navbar'
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
-      <Navbar />
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Button variant="contained" color="primary" component={Link} naked href="/">
+          Go to the main page
+        </Button>
         {/* <ProTip /> */}
         {/* <Copyright /> */}
       </Box>
