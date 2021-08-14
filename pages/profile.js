d45094cb1ae2f11c94b7d1bbcb943c9a9e0dd7d4
patjawat <patjawat@gmail.com>
2021-08-14 21:49:38 +0700

    update

--- pages/profile.js
+++ pages/profile.js
@@ -1,28 +1,106 @@
 import React,{useState,useEffect} from 'react'
-
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import axios  from 'axios';
-export default function Profile() {
+
+const MySwal = withReactContent(Swal)
+
+export default function Register() {
+    const [profile, setProfile] = useState({})
+    const [save, setSave] = useState({})
+
     const liffId = process.env.NEXT_PUBLIC_LIFF_ID
 
-    const [profile, setProfile] = useState({})
-    const [data, setData] = useState({})
-    const [error, setError] = useState({})
 
-    const getData = async () => {
-        const res = await axios.get(`${process.env.SHEET_USERS_API}/search?userId=*Ua45c4dcdc6ec65b8e9fff4a2693bcf72*`)
-        await setData(res.data)
-        console.log(res)
-    }
-      
+    // form validation rules 
+    const validationSchema = Yup.object().shape({
+        fname: Yup.string()
+            .required('ต้องระบุบ ชื่อ'),
+        lname: Yup.string()
+            .required('ต้องระบุบ นามสกุล'),
+        
+    });
+    const formOptions = { resolver: yupResolver(validationSchema) };
 
-    return (
-        <div>
-            {JSON.stringify(data,null,2)}
-            {JSON.stringify(error,null,2)}
+    
+    const onSubmit = data => {
+        try {
+            const res = axios.post(`${process.env.SHEET_USERS_API}`,data)
+            // console.log(res)
+            console.log(data)
+            MySwal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Your work has been saved',
+              showConfirmButton: false,
+              timer: 1500
+            }).then(()=>{
+              liff.closeWindow()
+            })
             
-            ข้อมูลส่วนตัว
+            setSave(data)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+    const { register, handleSubmit, reset, formState } = useForm(formOptions);
+    const { errors } = formState;
+
+
+    useEffect(async () => {
+      await reset({userId:profile.userId,displayName:profile.displayName,pictureUrl:profile.pictureUrl})
+      // const liff = (await import('@line/liff')).default
+      // await liff.ready
+      // const profile = await liff.getProfile()
+      // setProfile(profile)
+       const liff = (await import('@line/liff')).default
+      try {
+        await liff.init({ liffId });
+        const profile = await liff.getProfile()
+        await setProfile(profile)
+        console.log(profile)
+      } catch (error) {
+        console.error('liff init error', error.message)
+      }
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+    }, [profile.userId])
+  
+
+    
+    return (
+        <div className="container">
+
+            <h1 className="text-center">ลงทะเบียน</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="email" type="text" {...register('userId')}  className={`form-control ${errors.userId ? 'is-invalid' : ''}`} hidden="true"/>
+            <input name="email" type="text" {...register('displayName')} className={`form-control ${errors.displayName ? 'is-invalid' : ''}`}  hidden="true" />
+            <input name="email" type="text" {...register('pictureUrl')}  className={`form-control ${errors.pictureUrl ? 'is-invalid' : ''}`} hidden="true" />
+      
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">ชื่อ</label>
+            <input name="email" type="text" {...register('fname')} className={`form-control ${errors.fname ? 'is-invalid' : ''}`} />
+            <div className="invalid-feedback">{errors.fname?.message}</div>
         </div>
-    )
+
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">นามสกุล</label>
+            <input name="email" type="text" {...register('lname')} className={`form-control ${errors.lname ? 'is-invalid' : ''}`} />
+            <div className="invalid-feedback">{errors.lname?.message}</div>
+        </div>
+
+        <div className="form-group">
+            <label>โทรศัพท์</label>
+            <input name="email" type="text" {...register('phone')} className={`form-control ${errors.phone ? 'is-invalid' : ''}`} />
+            <div className="invalid-feedback">{errors.phone?.message}</div>
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-lg btn-block">ลงทะเบียน</button>
+        </form>
+      </div>
+    );
 }
-
-
