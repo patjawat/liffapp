a92c6ed9a8627325889acd27be67481a70733724
patjawat <patjawat@gmail.com>
2021-08-14 21:22:51 +0700

    update

--- pages/profile.js
+++ pages/profile.js
@@ -3,6 +3,7 @@ import React,{useState,useEffect} from 'react'
 import axios  from 'axios';
 export default function Profile() {
     const [profile, setProfile] = useState({})
+    const [data, setData] = useState({})
 
     useEffect(async () => {
 
@@ -12,6 +13,7 @@ export default function Profile() {
           const profile = await liff.getProfile()
           await setProfile(profile)
           console.log(profile)
+          await getData()
         } catch (error) {
           console.error('liff init error', error.message)
         }
@@ -20,10 +22,47 @@ export default function Profile() {
         }
       }, [profile.userId])
   
+
+    const getData = async () => {
+        const res = await axios.get(`${process.env.SHEET_USERS_API}/search?userId=*${useprofile.userIdrId}*`)
+    }
       
     return (
         <div>
+            {JSON.stringify(data,null,2)}
             ข้อมูลส่วนตัว
         </div>
     )
 }
+
+
+// export async function getServerSideProps({req,res}) {
+//     let headers = {}
+//     let me = {}
+  
+//     const session = await getSession({ req });
+//     if (session) {
+//       headers = {Authorization: `Bearer ${session.jwt}`};
+//     }else{
+//       res.writeHead(302, { Location: '/register' })
+//       res.end()
+//       return {}
+//     }
+//     let journals = [];
+  
+//     try {
+  
+//       let {data} = await axios.get(`${process.env.api}/profiles/me`, {
+//         headers: headers,
+//       })
+  
+    
+//       journals = data;
+//     } catch (e) {
+//       console.log('caught error');
+//       journals = [];
+//     }
+    
+//     return {props: {journals:journals,session:session }}  
+//   }
+
