2a85e9338e771716938ca0d5142385509d1bb58a
patjawat <patjawat@gmail.com>
2021-08-18 12:15:19 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        API:'http://abdad5d717da.ngrok.io/',
+        API:'https://abdad5d717da.ngrok.io/',
         NEXT_PUBLIC_LIFF_ID: '1656313134-na2Ad8MB',
         SHEET_USERS_API:"https://sheet.best/api/sheets/92e7d702-2e60-4c2a-8b33-1d4dacc5c5bc"
     },
