7735aa3c7f3ef9517718389836ab9098429a2898
patjawat <patjawat@gmail.com>
2021-08-18 14:57:45 +0700

    update

--- pages/profile.js
+++ pages/profile.js
@@ -6,6 +6,7 @@ export default function profile() {
 
     const router = useRouter()
     const [profile, setProfile] = useState({})
+    const [line, setLine] = useState({})
 
     const liffId = process.env.NEXT_PUBLIC_LIFF_ID
 
@@ -13,7 +14,7 @@ export default function profile() {
     const getMe = async () =>{
         try {
             const {data} = await axios.post(`${process.env.API}profiles/me`,{
-                id:"Ua45c4dcdc6ec65b8e9fff4a2693bcf72xx"
+                id:line.lineId
             })
             if(data.length==0){
                 router.push('/register')
@@ -25,25 +26,23 @@ export default function profile() {
         }
     }
 
+
     useEffect(async () => {
-        await getMe();
-    })
-    // useEffect(async () => {
-    //     await reset({line_id:profile.userId,displayName:profile.displayName,pictureUrl:profile.pictureUrl})
+        await reset({line_id:profile.userId,displayName:profile.displayName,pictureUrl:profile.pictureUrl})
       
-    //      const liff = (await import('@line/liff')).default
-    //     try {
-    //       await liff.init({ liffId });
-    //       const profile = await liff.getProfile()
-    //       await setProfile(profile)
-    //       await getMe()
-    //     } catch (error) {
-    //       console.error('liff init error', error.message)
-    //     }
-    //     if (!liff.isLoggedIn()) {
-    //       liff.login();
-    //     }
-    //   }, [profile.userId])
+         const liff = (await import('@line/liff')).default
+        try {
+          await liff.init({ liffId });
+          const profile = await liff.getProfile()
+          await setLine(profile)
+          await getMe()
+        } catch (error) {
+          console.error('liff init error', error.message)
+        }
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        }
+      }, [profile.userId])
 
 
 
