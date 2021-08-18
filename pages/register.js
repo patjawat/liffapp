bb8fe0041252e44be0490fb10ebfb90b1ec1cfd5
patjawat <patjawat@gmail.com>
2021-08-18 17:43:35 +0700

    update

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
-import { useForm } from "react-hook-form";
+import Select from "react-select";
+import { useForm,Controller } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import Swal from 'sweetalert2'
@@ -10,6 +11,7 @@ const MySwal = withReactContent(Swal)
 
 export default function Register() {
     const [profile, setProfile] = useState({})
+    const [categorisClass, setCategorisClass] = useState([])
     const [save, setSave] = useState({})
 
     const liffId = process.env.NEXT_PUBLIC_LIFF_ID
@@ -46,9 +48,24 @@ export default function Register() {
             console.log(error)
         }
     };
-    const { register, handleSubmit, reset, formState } = useForm(formOptions);
+
+
+    const getCategories = async () => {
+        const {data} = await axios.get(`${process.env.API}categoris-types`)
+        await setCategorisClass(data[0].ids);
+        // await setCategorisClass([
+        //   ...new Set(data.map((item, i) => item.name)),
+        // ]);
+        console.log(data[0].ids)
+    }
+
+    const { register, handleSubmit, reset, formState,control } = useForm(formOptions);
     const { errors } = formState;
 
+
+    // useEffect(async () => {
+    //   await getCategories()
+    // },[])
 
     useEffect(async () => {
       await reset({line_id:profile.userId,displayName:profile.displayName,pictureUrl:profile.pictureUrl})
@@ -58,6 +75,8 @@ export default function Register() {
         await liff.init({ liffId });
         const profile = await liff.getProfile()
         await setProfile(profile)
+        await getCategories()
+
         console.log(profile)
       } catch (error) {
         console.error('liff init error', error.message)
@@ -71,7 +90,6 @@ export default function Register() {
     
     return (
         <div className="container">
-{/* {JSON.stringify(profile)} */}
             <h1 className="text-center">ลงทะเบียน</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
             <input name="email" type="text" {...register('line_id')}  className={`form-control ${errors.line_id ? 'is-invalid' : ''}`} hidden="true"/>
@@ -90,12 +108,69 @@ export default function Register() {
             <div className="invalid-feedback">{errors.lname?.message}</div>
         </div>
 
+
+        <div className="form-group">
+        <label>สังกัด</label>
+        <Controller
+        name="iceCreamType"
+        control={control}
+        render={({ field }) => <Select 
+          {...field} 
+          options={categorisClass.map((option) => ({
+            ...option,
+            label: option.title,
+            value: option.id,
+            clearableValue: true
+        }))}
+        />}
+      />
+        </div>
+
         <div className="form-group">
             <label>โทรศัพท์</label>
             <input name="email" type="text" {...register('phone')} className={`form-control ${errors.phone ? 'is-invalid' : ''}`} />
             <div className="invalid-feedback">{errors.phone?.message}</div>
         </div>
 
+        <div className="form-group">
+        <label>ยื่นขอในตำแหน่ง</label>
+        <Controller
+        name="iceCreamType"
+        control={control}
+        render={({ field }) => <Select 
+          {...field} 
+          options={categorisClass.map((option) => ({
+            ...option,
+            label: option.title,
+            value: option.id,
+            clearableValue: true
+        }))}
+        />}
+      />
+        </div>
+
+        <div className="form-group">
+        <label>สาขาวิชา</label>
+        <Controller
+        name="iceCreamType"
+        control={control}
+        render={({ field }) => <Select 
+          {...field} 
+          options={categorisClass.map((option) => ({
+            ...option,
+            label: option.title,
+            value: option.id,
+            clearableValue: true
+        }))}
+        />}
+      />
+        </div>
+
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">email(ถ้ามี)</label>
+            <input name="email" type="text" {...register('lname')} className={`form-control ${errors.lname ? 'is-invalid' : ''}`} />
+            <div className="invalid-feedback">{errors.lname?.message}</div>
+        </div>
         <button type="submit" className="btn btn-primary btn-lg btn-block">ลงทะเบียน</button>
         </form>
       </div>
